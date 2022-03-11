f34b6ee169688ed4be9a7ad9cceff9299018e555
Andres fleipe <101063020+liblack99@users.noreply.github.com>
2022-03-11 17:19:04 -0500

    Add files via upload

---
+++ fizz.js
@@ -0,0 +1,18 @@
+var numeros = 100;
+for( var i = 1; i <=100; i++)
+{
+  if(i % 3 == 0)
+  {
+  document.write("fizz");
+  }
+
+  if (i % 5 == 0)
+  {
+    document.write("buzz");
+  }
+  else
+  {
+  document.write(i);
+  }
+  document.write("<br />");
+}
